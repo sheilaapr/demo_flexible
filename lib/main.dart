7ef855c146efecfb654a97f9bb9b6206901d358e
sheilaapr <230605110005@student.uin-malang.ac.id>
2025-09-16 23:17:37 +0700

    Pesan commit pertama

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,40 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flexible'),
         ),
-        body: Center(
-          child: Text('Hello, FLutter!'),
+        body: const Center(
+          child: Row(
+            children: [
+              Flexible(
+                fit: FlexFit.tight, flex: 1, child: KotakBiruJempolKecil()),
+              Flexible(
+                fit: FlexFit.tight, flex: 2, child: KotakBiruJempolKecil()),
+              Flexible(
+                fit: FlexFit.loose, flex: 3, child: KotakBiruJempolKecil()),
+            ],
+          )
         ),
+      ),
+    );
+  }
+}
+
+class KotakBiruJempolKecil extends StatelessWidget {
+  const KotakBiruJempolKecil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 75,
+      height: 75,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(color: Colors.black, width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Icon(
+        Icons.thumb_up,
+        color: Colors.white,
+        size: 40,
       ),
     );
   }
